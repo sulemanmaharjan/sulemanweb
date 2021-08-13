6bce42a5ac7c29e8f5852335fccdea5e77abf7d3
sulemanmaharjan1@gmail.com <sulemanmaharjan1@gmail.com>
2021-08-13 12:25:59 +0545

    hgjht

--- deepika.php/login1.php
+++ deepika.php/login1.php
@@ -1,16 +1,18 @@
 <?php
-
+$servername = "localhost";
+$username = "username";
+$password = "password";
 $dbname = "user";
 
 // Create connection
-$conn = new mysqli("localhost", "root", "", $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 $u=$_REQUEST["username"];
 $p=$_REQUEST["password"];
-$sql = "SELECt * from login where username=? and password=?";
+$sql = "SELECT * from login where username=? and password=?";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("ss", $u, $p);
 $stmt->execute();
@@ -18,11 +20,10 @@ $result = $stmt->get_result();
 
 
 if ($result->num_rows > 0) {
-echo "successfully logged in";
-}
-else{
-    echo "login is unsuccesful";
-}
-?>
-
-hello hoe are you? 
+    echo "successfully logged in";
+    }
+    else{
+        echo "login is unsuccesful";
+    }
+    ?>
+?> 
---
+++ sulemanphp/login1.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <!-- jquery cdn -->
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../bootstrap5/css/bootstrap.css">
+    <style>
+        h1{
+            margin-bottom:10px;
+            color:gray;
+            font-family: "Times New Roman";
+        }
+        form{
+            width:400px;
+        }
+        </style>
+</head>
+<body>
+    <div class="container jumbotron card ">
+        <h1>Login to  our website</h1>
+    <form action="loginprocess.php" >
+        <div class="form-group">
+            
+            <input placeholder="Please enter your username" name="username" type="text" class="form-control" id="email">
+            <br />
+            <input placeholder="Please enter your password" name="password" type="password" class="form-control" id="email">
+          </div>
+         <input type="submit" value="Login" class="btn btn-success m-3">
+          
+    </form>
+    </div>
+</body>
+</html>
